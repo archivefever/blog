274157c876a0499be143769af2368e62be5b3c3b
Marla Kallstrom <brandykou@gmail.com>
2017-09-17 18:17:55 -0500

    add views for editing posts and fix delete routing errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,36 +4,55 @@ class PostsController < ApplicationController
     @posts = Post.order(:created_at)
   end
 
-  def new
-
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to :action=>'show'
-    else
-      render :action=>'new'
-  end
-
   def show
     @post = Post.find(params[:id])
   end
 
-  def edit
+  before_filter :authorize
 
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to url: {action: "show"}
+    else
+      render :action=>'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    authorize_post_action(@post)
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    authorize_post_action(@post)
+    @post.update_attributes(post_params)
+    if @post.save
+      redirect_to url: {action: "show"}
+    else
+      render :action=>'edit'
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    authorize_post_action(@post)
+    @post.destroy
+    redirect_to '/posts'
   end
+
+  private
 
   def post_params
     params.require(:post).permit(:title, :body)
   end
 
+   def authorize_post_action(post)
+    redirect to '/posts' unless current_user.id == post.user_id
+  end
 end
