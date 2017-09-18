2349c01c91d9d4def9b6aeb2a856444c5dd86d29
Marla Kallstrom <brandykou@gmail.com>
2017-09-17 19:29:57 -0500

    Add comments routes and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,14 @@
   before_filter :authorize
 
   def new
+    @post = Post.find(params[:post_id])
   end
 
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_to url: {action: "show"}
+      redirect_to post_url(@comment.post_id)
     else
       render :action=>'new'
     end
@@ -25,7 +26,7 @@
     authorize_comment_action(@comment)
     @comment.update_attributes(comment_params)
     if @comment.save
-      redirect_to '/posts/<%= @comment.post_id %>'
+      redirect_to post_url(@comment.post_id)
     else
       render :action=>'edit'
     end
@@ -35,16 +36,16 @@
     @comment = Comment.find(params[:id])
     authorize_comment_action(@comment)
     @comment.destroy
-    redirect_to '/comments'
+    redirect_to '/posts'
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:title, :body)
+    params.require(:comment).permit(:title, :body, :post_id)
   end
 
    def authorize_comment_action(comment)
-    redirect to '/comments' unless current_user.id == comment.user_id
+    redirect to '/posts' unless current_user.id == comment.user_id
   end
 end
